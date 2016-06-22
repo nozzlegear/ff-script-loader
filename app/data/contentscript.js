c7c5341236563548bfed0107576060e3761f32e3
Joshua Harms <joshua@nozzlegear.com>
2016-06-22 16:17:57 -0500

    Do not try to create a Deliveron client.

--- app/data/contentscript.js
+++ app/data/contentscript.js
@@ -5,20 +5,7 @@ script.type = "text/javascript";
 script.src = "https://localhost:3001/dist/index.js";
 script.onload = () =>
 {
-    console.log("Deliveron Client script loaded into DOM from localhost:3001.");
-    
-    const config = {
-        label: "Pick your delivery date:",
-        format: "mm/dd/yyyy",
-        addPickerToCheckout: false,
-        allowChangeFromCheckout: false,
-    }
-
-    const client = new DeliverOn.Client(config);
-
-    window["deli"] = client;
-
-    console.log("Configured Deliveron Client available to window as 'deli' variable.");
+    console.log("Deliveron Client script loaded into DOM from localhost:3001 via ff-script-loader v0.1.2.");
 }
 
 document.body.appendChild(script);
